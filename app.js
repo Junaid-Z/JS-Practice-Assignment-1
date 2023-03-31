e9f77b2dfbad23597e321fb2c67d0894c2f5877f
Junaid-Z <jzafar828@gmail.com>
2023-03-31 23:28:20 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//vars
+var a = 1;
+var b = 3;
+var c = 5;
+//logging
+console.log("Sum of the numbers " + a + " " + b + " " + c + " is " + (a + b + c));
+//prompts
+// var userName = prompt("Enter your name");
+// var userCity = prompt("Enter your city");
+// //alerts
+// alert("Hello " + userName + " from " + userCity);
+a = "11";
+b = "9";
+console.log("Product of " + a + " " + b + " is " + (a * b));
+console.log("Differnce between " + a + " and " + b + " is " + (a - b));
+
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+//string concatenation
+console.log(a + b);
+
+var d = 6;
+var e = 0;
+var f = 12;
+console.log(a + b - c * d / f + e);
+console.log(a++)
+console.log(++a)
+console.log(a--)
+console.log(--a)
+a = a + 1
+console.log(a)
